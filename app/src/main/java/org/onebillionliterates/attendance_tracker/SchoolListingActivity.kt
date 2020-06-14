75e65b2c5fc5ec5fc2378b70bfd65f7b48dc9094
Y Ramesh Rao <yrameshrao@gmail.com>
2020-06-14 06:54:27 +0530

    Leku LocationPicker integrated

--- app/src/main/java/org/onebillionliterates/attendance_tracker/SchoolListingActivity.kt
+++ app/src/main/java/org/onebillionliterates/attendance_tracker/SchoolListingActivity.kt
@@ -1,11 +1,22 @@
 package org.onebillionliterates.attendance_tracker
 
+import android.app.Activity
+import android.app.Activity.*
+import android.content.Context
 import android.content.Intent
+import android.location.Address
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.maps.model.LatLng
+import com.schibstedspain.leku.*
+import com.schibstedspain.leku.locale.SearchZoneRect
+import com.schibstedspain.leku.tracker.LocationPickerTracker
+import com.schibstedspain.leku.tracker.TrackEvents
 import org.onebillionliterates.attendance_tracker.adapter.SchoolsAdapter
 import org.onebillionliterates.attendance_tracker.model.School
 
@@ -20,12 +31,40 @@ class SchoolListingActivity : AppCompatActivity() {
 
         val buttonAddNew = findViewById<Button>(R.id.add_school_button)
         buttonAddNew.setOnClickListener {
-            val intent = Intent(this, AddSchoolActivity::class.java)
-            startActivity(intent)
+            val locationPickerIntent = LocationPickerActivity.Builder()
+                .withLocation(12.9542946, 77.4908551)
+                .withSearchZone("en_IN")
+                .build(applicationContext)
+
+            startActivityForResult(locationPickerIntent, MAP_BUTTON_REQUEST_CODE)
         }
     }
 
-    private fun initListContainer(listContainer : RecyclerView){
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            Log.d("RESULT****", "OK")
+            val latitude = data.getDoubleExtra(LATITUDE, 0.0)
+            Log.d("LATITUDE****", latitude.toString())
+            val longitude = data.getDoubleExtra(LONGITUDE, 0.0)
+            Log.d("LONGITUDE****", longitude.toString())
+            val address = data.getStringExtra(LOCATION_ADDRESS)
+            Log.d("ADDRESS****", address.toString())
+            val postalcode = data.getStringExtra(ZIPCODE)
+            Log.d("POSTALCODE****", postalcode.toString())
+            val fullAddress = data.getParcelableExtra<Address>(ADDRESS)
+            if (fullAddress != null) {
+                Log.d("FULL ADDRESS****", fullAddress.toString())
+            }
+        }
+        if (resultCode == Activity.RESULT_CANCELED) {
+            Log.d("RESULT****", "CANCELLED")
+        }
+    }
+
+
+    private fun initListContainer(listContainer: RecyclerView) {
         listContainer.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
 
         val dummySchools = ArrayList<School>()
@@ -38,5 +77,9 @@ class SchoolListingActivity : AppCompatActivity() {
 
         val adapter = SchoolsAdapter(dummySchools)
         listContainer.adapter = adapter
+    }
+
+    companion object {
+        private const val MAP_BUTTON_REQUEST_CODE = 1
     }
 }
