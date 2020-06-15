96107916dca0ab383a5de2c13295a1f05e159966
Y Ramesh Rao <yrameshrao@gmail.com>
2020-06-15 05:59:09 +0530

    School Location Manipulations Added

--- app/src/main/java/org/onebillionliterates/attendance_tracker/AddSchoolActivity.kt
+++ app/src/main/java/org/onebillionliterates/attendance_tracker/AddSchoolActivity.kt
@@ -1,15 +1,99 @@
 package org.onebillionliterates.attendance_tracker
 
+import android.app.Activity
 import android.content.Intent
+import android.location.Address
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.schibstedspain.leku.*
 import kotlinx.android.synthetic.main.school_create.*
-import kotlinx.android.synthetic.main.school_listing.*
+import org.onebillionliterates.attendance_tracker.drawables.EmailDrawable
+import org.onebillionliterates.attendance_tracker.drawables.MobileDrawable
+import org.onebillionliterates.attendance_tracker.drawables.SchoolSolidDrawable
 
 class AddSchoolActivity : AppCompatActivity() {
+    companion object {
+        private const val MAP_BUTTON_REQUEST_CODE = 1
+        private val DEFAULT_LOC = LatLng(12.9542946, 77.4908551)
+    }
+
+    private lateinit var embeddedMap:GoogleMap;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.school_create)
+
+        initView()
+    }
+
+
+    private fun initView() {
+        schoolNameIcon.setImageDrawable(SchoolSolidDrawable(this))
+        phoneIcon.setImageDrawable(MobileDrawable(this))
+        emailIcon.setImageDrawable(EmailDrawable(this))
+        searchSchool.setOnClickListener {
+            val locationPickerIntent = LocationPickerActivity.Builder()
+                .withLocation(DEFAULT_LOC)
+                .withSearchZone("en_IN")
+                .build(applicationContext)
+
+            startActivityForResult(locationPickerIntent, MAP_BUTTON_REQUEST_CODE)
+        }
+
+        addLocationToMap()
+    }
+
+    private fun addLocationToMap(position: LatLng = DEFAULT_LOC) {
+        val mapFragment = schoolAddressDescription as SupportMapFragment
+        with(mapFragment) {
+            onCreate(null)
+            getMapAsync() {
+                MapsInitializer.initialize(applicationContext)
+                setMapLocation(it, position, "Bangalore")
+            }
+        }
+    }
+
+    private fun setMapLocation(map: GoogleMap, position: LatLng, snippet:String) {
+        with(map) {
+            embeddedMap = map
+            moveCamera(CameraUpdateFactory.newLatLngZoom(position, 13f))
+            addMarker(MarkerOptions().position(position).snippet(snippet))
+            mapType = GoogleMap.MAP_TYPE_NORMAL
+            setOnMapClickListener {
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            Log.d("RESULT****", "OK")
+            val latitude = data.getDoubleExtra(LATITUDE, DEFAULT_LOC.latitude)
+            Log.d("LATITUDE****", latitude.toString())
+            val longitude = data.getDoubleExtra(LONGITUDE, DEFAULT_LOC.longitude)
+            Log.d("LONGITUDE****", longitude.toString())
+            val address = data.getStringExtra(LOCATION_ADDRESS)
+            Log.d("ADDRESS****", address.toString())
+            val postalcode = data.getStringExtra(ZIPCODE)
+            Log.d("POSTALCODE****", postalcode.toString())
+            val fullAddress = data.getParcelableExtra<Address>(ADDRESS)
+            if (fullAddress != null) {
+                Log.d("FULL ADDRESS****", fullAddress.toString())
+            }
+
+            setMapLocation(embeddedMap, position = LatLng(latitude, longitude), snippet = address)
+        }
+        if (resultCode == Activity.RESULT_CANCELED) {
+            Log.d("RESULT****", "CANCELLED")
+        }
     }
 }
--- app/src/main/java/org/onebillionliterates/attendance_tracker/SchoolListingActivity.kt
+++ app/src/main/java/org/onebillionliterates/attendance_tracker/SchoolListingActivity.kt
@@ -1,22 +1,11 @@
 package org.onebillionliterates.attendance_tracker
 
-import android.app.Activity
-import android.app.Activity.*
-import android.content.Context
 import android.content.Intent
-import android.location.Address
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.gms.maps.model.LatLng
-import com.schibstedspain.leku.*
-import com.schibstedspain.leku.locale.SearchZoneRect
-import com.schibstedspain.leku.tracker.LocationPickerTracker
-import com.schibstedspain.leku.tracker.TrackEvents
+import kotlinx.android.synthetic.main.school_listing.*
 import org.onebillionliterates.attendance_tracker.adapter.SchoolsAdapter
 import org.onebillionliterates.attendance_tracker.model.School
 
@@ -29,37 +18,9 @@ class SchoolListingActivity : AppCompatActivity() {
         val listContainer = findViewById<RecyclerView>(R.id.school_list_container)
         initListContainer(listContainer)
 
-        val buttonAddNew = findViewById<Button>(R.id.add_school_button)
-        buttonAddNew.setOnClickListener {
-            val locationPickerIntent = LocationPickerActivity.Builder()
-                .withLocation(12.9542946, 77.4908551)
-                .withSearchZone("en_IN")
-                .build(applicationContext)
-
-            startActivityForResult(locationPickerIntent, MAP_BUTTON_REQUEST_CODE)
-        }
-    }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && data != null) {
-            Log.d("RESULT****", "OK")
-            val latitude = data.getDoubleExtra(LATITUDE, 0.0)
-            Log.d("LATITUDE****", latitude.toString())
-            val longitude = data.getDoubleExtra(LONGITUDE, 0.0)
-            Log.d("LONGITUDE****", longitude.toString())
-            val address = data.getStringExtra(LOCATION_ADDRESS)
-            Log.d("ADDRESS****", address.toString())
-            val postalcode = data.getStringExtra(ZIPCODE)
-            Log.d("POSTALCODE****", postalcode.toString())
-            val fullAddress = data.getParcelableExtra<Address>(ADDRESS)
-            if (fullAddress != null) {
-                Log.d("FULL ADDRESS****", fullAddress.toString())
-            }
-        }
-        if (resultCode == Activity.RESULT_CANCELED) {
-            Log.d("RESULT****", "CANCELLED")
+        addNewSchool.setOnClickListener {
+            val intent = Intent(this, AddSchoolActivity::class.java)
+            startActivity(intent)
         }
     }
 
@@ -77,9 +38,5 @@ class SchoolListingActivity : AppCompatActivity() {
 
         val adapter = SchoolsAdapter(dummySchools)
         listContainer.adapter = adapter
-    }
-
-    companion object {
-        private const val MAP_BUTTON_REQUEST_CODE = 1
     }
 }
